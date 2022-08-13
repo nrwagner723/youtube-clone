2c15540d41da270ffd65670f5921e43105f97b58
Natalie Wagner <nrwagner723@gmail.com>
2022-08-12 19:15:07 -0500

    began working on get/post requests

--- backend/comments/serializers.py
+++ backend/comments/serializers.py
@@ -4,4 +4,4 @@ from .models import Comment
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = ['id', 'user', 'video_id', 'likes', 'dislikes']
+        fields = ['id', 'user', 'video_id', 'text', 'likes', 'dislikes']
