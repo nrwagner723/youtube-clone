b83aad0b80c76400bad127ca5a316dd2b12eaa3e
Natalie Wagner <nrwagner723@gmail.com>
2022-08-18 20:55:25 -0500

    started changing some of the starter code to make more sense for comments

--- backend/drf_jwt_backend/urls.py
+++ backend/drf_jwt_backend/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
     path('api/cars/', include('cars.urls')),
-    path('comments/', include('comments.urls')),
+    path('api/comments/', include('comments.urls')),
 ]
--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -9,30 +9,30 @@ const HomePage = () => {
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
   //TODO: Add an AddCars Page to add a car for a logged in user's garage
   const [user, token] = useAuth();
-  const [cars, setCars] = useState([]);
+  const [comments, setComments] = useState([]);
 
   useEffect(() => {
-    const fetchCars = async () => {
+    const fetchComments = async () => {
       try {
-        let response = await axios.get("http://127.0.0.1:8000/api/cars/", {
+        let response = await axios.get("http://127.0.0.1:8000/api/comments/", {
           headers: {
             Authorization: "Bearer " + token,
           },
         });
-        setCars(response.data);
+        setComments(response.data);
       } catch (error) {
-        console.log(error.response.data);
+        console.log(error.response);
       }
     };
-    fetchCars();
+    fetchComments();
   }, [token]);
   return (
     <div className="container">
       <h1>Home Page for {user.username}!</h1>
-      {cars &&
-        cars.map((car) => (
-          <p key={car.id}>
-            {car.year} {car.model} {car.make}
+      {comments &&
+        comments.map((comment) => (
+          <p key={comment.id}>
+            {/* {car.year} {car.model} {car.make} */}
           </p>
         ))}
     </div>
